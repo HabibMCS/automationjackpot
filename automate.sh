7625b4a9dd256b7fb4f8e7742fc13dc86bb2ab06
HabibMCS <77892294+HabibMCS@users.noreply.github.com>
2024-06-27 23:09:27 +0500

    Update automate.sh

--- automate.sh
+++ automate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# install wine
+# Install Wine
 sudo dpkg --add-architecture i386
 sudo mkdir -pm755 /etc/apt/keyrings
 sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
@@ -8,28 +8,49 @@ sudo apt update
 sudo apt install --install-recommends winehq-stable
 wine --version
 
-
+# Create directories
 cd ~
-
 mkdir -p jackpot
 
+# Download and unzip the game
 cd jackpot
-
-wget -O game.zip https://file.io/NlhcuXwEeqO9
-
+wget -O game.zip https://file.io/m5l8QNOe99Ll
 unzip game.zip
 
-mkdir -p ~/.config/autostart/automation
-mv jackpot.desktop ~/.config/autostart/automation/
-chmod +x ~/.config/autostart/automation/jackpot.desktop
+# Move run_test.sh to the correct location and make it executable
 chmod +x ~/jackpot/run_test.sh
 
+# Install Python requirements
 if [ -f requirements.txt ]; then
     sudo pip3 install -r requirements.txt
 else
     sudo pip3 install --upgrade pip
     sudo pip3 install pygame
     sudo pip3 install gif-pygame
+    sudo pip3 install tk
 fi
+
+# Create the systemd service file
+sudo bash -c 'cat << EOF > /etc/systemd/system/pygame_gui.service
+[Unit]
+Description=Run Pygame GUI at startup
+After=multi-user.target
+
+[Service]
+Type=simple
+ExecStart=/home/jorge/jackpot/run_test.sh
+Restart=on-failure
+User=jorge
+Environment=DISPLAY=:0
+Environment=XAUTHORITY=/run/user/1000/gdm/Xauthority
+
+[Install]
+WantedBy=multi-user.target
+EOF'
+
+# Enable and start the service
+sudo systemctl daemon-reload
+sudo systemctl enable pygame_gui.service
+sudo systemctl start pygame_gui.service
 
 echo "Setup complete."
