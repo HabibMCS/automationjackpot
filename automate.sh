d9419ea80f369ac5df962370d6d2bfe680d95f5d
HabibMCS <77892294+HabibMCS@users.noreply.github.com>
2024-06-28 18:01:24 +0500

    Update automate.sh

--- automate.sh
+++ automate.sh
@@ -8,19 +8,22 @@ sudo apt update
 sudo apt install --install-recommends winehq-stable
 wine --version
 
+# Get the original user
+ORIGINAL_USER=${SUDO_USER:-$(whoami)}
+
 # Create directories
 cd ..
-mkdir jackpot
-sudo chown -R $USER:$USER /home/$USER
-sudo chown -R $USER:$USER /home/$USER/jackpot
+mkdir -p /home/$ORIGINAL_USER/jackpot
+sudo chown -R $ORIGINAL_USER:$ORIGINAL_USER /home/$ORIGINAL_USER
+sudo chown -R $ORIGINAL_USER:$ORIGINAL_USER /home/$ORIGINAL_USER/jackpot
 
 # Download and unzip the game
-cd jackpot
+cd /home/$ORIGINAL_USER/jackpot
 wget -O game.zip "https://nustedupk0-my.sharepoint.com/:u:/g/personal/hsolangi_bee-57mcs_student_nust_edu_pk/EZkdpRMcwK5CsdGqa3ao2swBwmNSfhSpjwPi_ToC_mrkDg?e=ApaTlI&download=1"
 unzip game.zip
 
 # Move run_test.sh to the correct location and make it executable
-sed -i 's/\r$//' /home/$USER/jackpot/run_test.sh
+sed -i 's/\r$//' /home/$ORIGINAL_USER/jackpot/run_test.sh
 chmod +x ./run_test.sh
 
 # Install Python and Tkinter
@@ -42,9 +45,9 @@ After=multi-user.target
 
 [Service]
 Type=simple
-ExecStart=/home/$USER/jackpot/run_test.sh
-Restart=always
-User=$USER
+ExecStart=/home/$ORIGINAL_USER/jackpot/run_test.sh
+Restart=on-failure
+User=$ORIGINAL_USER
 Environment=DISPLAY=:0
 Environment=XAUTHORITY=/run/user/1000/gdm/Xauthority
 
