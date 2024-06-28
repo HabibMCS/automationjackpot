b7d7d05d2150ac9f7eef12beb72426c944e79bcd
HabibMCS <77892294+HabibMCS@users.noreply.github.com>
2024-06-28 16:59:20 +0500

    Update automate.sh

--- automate.sh
+++ automate.sh
@@ -11,42 +11,46 @@ wine --version
 # Create directories
 cd ..
 mkdir jackpot
+sudo chown -R $USER:$USER /home/$USER
+sudo chown -R $USER:$USER /home/$USER/jackpot
 
 # Download and unzip the game
 cd jackpot
-wget -O game.zip https://nustedupk0-my.sharepoint.com/:u:/g/personal/hsolangi_bee-57mcs_student_nust_edu_pk/EZkdpRMcwK5CsdGqa3ao2swBwmNSfhSpjwPi_ToC_mrkDg?e=ApaTlI&download=1
+wget -O game.zip "https://nustedupk0-my.sharepoint.com/:u:/g/personal/hsolangi_bee-57mcs_student_nust_edu_pk/EZkdpRMcwK5CsdGqa3ao2swBwmNSfhSpjwPi_ToC_mrkDg?e=ApaTlI&download=1"
 unzip game.zip
 
 # Move run_test.sh to the correct location and make it executable
+sed -i 's/\r$//' /home/$USER/jackpot/run_test.sh
 chmod +x ./run_test.sh
-sudo apt install python3-pip
+
+# Install Python and Tkinter
+sudo apt install -y python3-pip python3-tk
+
 # Install Python requirements
 if [ -f requirements.txt ]; then
     sudo pip3 install -r requirements.txt
 else
     sudo pip3 install --upgrade pip
-    sudo pip3 install pygame
-    sudo pip3 install gif-pygame
-    sudo pip3 install tk
+    sudo pip3 install pygame gif-pygame
 fi
 
 # Create the systemd service file
-sudo bash -c 'cat << EOF > /etc/systemd/system/pygame_gui.service
+sudo bash -c "cat << EOF > /etc/systemd/system/pygame_gui.service
 [Unit]
 Description=Run Pygame GUI at startup
 After=multi-user.target
 
 [Service]
 Type=simple
-ExecStart=/home/tcm/jackpot/run_test.sh
-Restart=on-failure
-User=jorge
+ExecStart=/home/$USER/jackpot/run_test.sh
+Restart=always
+User=$USER
 Environment=DISPLAY=:0
 Environment=XAUTHORITY=/run/user/1000/gdm/Xauthority
 
 [Install]
 WantedBy=multi-user.target
-EOF'
+EOF"
 
 # Enable and start the service
 sudo systemctl daemon-reload
