ac13909d28e56409bd37c574b5c75eaa4037b561
HabibMCS <77892294+HabibMCS@users.noreply.github.com>
2024-07-01 00:55:45 +0500

    Update automateexe.sh

--- automateexe.sh
+++ automateexe.sh
@@ -4,20 +4,18 @@
 ORIGINAL_USER=${SUDO_USER:-$(whoami)}
 
 # Create directories
-cd /home
-mkdir -p /home/jackpot
-sudo chown -R $ORIGINAL_USER:$ORIGINAL_USER /home/
-sudo chown -R jorge:jorge /home/
-sudo chown -R tcm:tcm /home/
-sudo chown -R $ORIGINAL_USER:$ORIGINAL_USER /home/jackpot
+cd ..
+mkdir -p /home/$ORIGINAL_USER/jackpot
+sudo chown -R $ORIGINAL_USER:$ORIGINAL_USER /home/$ORIGINAL_USER
+sudo chown -R $ORIGINAL_USER:$ORIGINAL_USER /home/$ORIGINAL_USER/jackpot
 
 # Download and unzip the game
-cd /home/jackpot
+cd /home/$ORIGINAL_USER/jackpot
 wget -O gameexe.zip "https://nustedupk0-my.sharepoint.com/:u:/g/personal/hsolangi_bee-57mcs_student_nust_edu_pk/EbYMiTKOiL9HlAejzyOaYPsBQdGnW9Bk_R0JELwHBZA5vg?e=nvgtV2&download=1"
 unzip gameexe.zip
 
 # Move run_test.sh to the correct location and make it executable
-sed -i 's/\r$//' /home/jackpot/run_test.sh
+sed -i 's/\r$//' /home/$ORIGINAL_USER/jackpot/run_test.sh
 chmod +x ./run_test.sh
 
 # Install Python and Tkinter
@@ -39,8 +37,9 @@ After=multi-user.target
 
 [Service]
 Type=simple
-ExecStart=/home/jackpot/run_test.sh
+ExecStart=/home/$ORIGINAL_USER/jackpot/run_test.sh
 Restart=on-failure
+User=$ORIGINAL_USER
 Environment=DISPLAY=:0
 Environment=XAUTHORITY=/run/user/1000/gdm/Xauthority
 
